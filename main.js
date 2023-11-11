2c60b510f6a41c7005c6e613ea97a0f0c0baf9f5
Mahmoud Mohsen <42476566+mody120@users.noreply.github.com>
2023-11-11 10:56:49 +0200

    test main.js

--- main.js
+++ main.js
@@ -1,99 +1,99 @@
 console.log("connected")
-function getTimesPrayer (city, country){
-  // get the dates
-  const date = new Date()
-  const dateByYear = date.getFullYear()
-  const dateByMonth = date.getMonth();
-  const dateByDay = date.getDate() - 1
+// function getTimesPrayer (city, country){
+//   // get the dates
+//   const date = new Date()
+//   const dateByYear = date.getFullYear()
+//   const dateByMonth = date.getMonth();
+//   const dateByDay = date.getDate() - 1
 
-  // Reques the data from the api
-  axios
-    .get(
-      `http://api.aladhan.com/v1/calendarByCity/${dateByYear}/${
-        dateByMonth + 1
-      }?city=${city}&country=${country}&method=2`
-    )
-    .then((response) => {
-      const dates = response.data.data;
-      const hijriDate = dates[dateByDay].date.hijri;
-      const gregorianDate = dates[dateByDay].date.gregorian;
-      const hijri =
-        hijriDate.weekday.ar +
-        " " +
-        hijriDate.day +
-        " " +
-        hijriDate.month.ar +
-        " " +
-        hijriDate.year; 
-
-
-      const gregorian =
-        gregorianDate.weekday.en +
-        " " +
-        gregorianDate.day +
-        " " +
-        gregorianDate.month.en +
-        " " +
-        gregorianDate.year; 
-      let { Sunrise, Fajr, Dhuhr, Asr, Maghrib, Isha } = dates[dateByDay].timings;
+//   // Reques the data from the api
+//   axios
+//     .get(
+//       `http://api.aladhan.com/v1/calendarByCity/${dateByYear}/${
+//         dateByMonth + 1
+//       }?city=${city}&country=${country}&method=2`
+//     )
+//     .then((response) => {
+//       const dates = response.data.data;
+//       const hijriDate = dates[dateByDay].date.hijri;
+//       const gregorianDate = dates[dateByDay].date.gregorian;
+//       const hijri =
+//         hijriDate.weekday.ar +
+//         " " +
+//         hijriDate.day +
+//         " " +
+//         hijriDate.month.ar +
+//         " " +
+//         hijriDate.year; 
 
 
-      Sunrise = Sunrise.split(" ")[0];
-      Fajr = Fajr.split(" ")[0];
-      Dhuhr = Dhuhr.split(" ")[0];
-      Asr = Asr.split(" ")[0];
-      Maghrib = Maghrib.split(" ")[0];
-      Isha = Isha.split(" ")[0];
-
-      displayTimes(Sunrise, Fajr, Dhuhr, Asr, Maghrib, Isha,gregorian, hijri);
-    })
-    .catch(error => console.log(error + ' @Getting Time Prayer Func'))
-}
+//       const gregorian =
+//         gregorianDate.weekday.en +
+//         " " +
+//         gregorianDate.day +
+//         " " +
+//         gregorianDate.month.en +
+//         " " +
+//         gregorianDate.year; 
+//       let { Sunrise, Fajr, Dhuhr, Asr, Maghrib, Isha } = dates[dateByDay].timings;
 
 
-function displayTimes(
-  Sunrise,
-  Fajr,
-  Dhuhr,
-  Asr,
-  Maghrib,
-  Isha,
-  gregorian,
-  hijri
-) {
-  let sunriseElement = document.querySelector(".sunrise .time");
-  let fajrElement = document.querySelector(".fajr .time");
-  let dhuhrElement = document.querySelector(".dhuhr .time");
-  let asrElement = document.querySelector(".asr .time");
-  let maghribElement = document.querySelector(".maghrib .time");
-  let ishaElement = document.querySelector(".isha .time");
-  let gregorianElement = document.querySelector(".gregorian-date");
-  let hijriElement = document.querySelector(".hijri-date");
+//       Sunrise = Sunrise.split(" ")[0];
+//       Fajr = Fajr.split(" ")[0];
+//       Dhuhr = Dhuhr.split(" ")[0];
+//       Asr = Asr.split(" ")[0];
+//       Maghrib = Maghrib.split(" ")[0];
+//       Isha = Isha.split(" ")[0];
 
-  let arr = [Sunrise, Fajr, Dhuhr, Asr, Maghrib, Isha];
-  const resultArr = arr.map((item) => {
-    const hours = Number(item.slice(0, 2));
-    const minutes = item.slice(2);
-    return hours > 12 ? hours - 12 + minutes + " PM" : item + " AM";
-  });
+//       displayTimes(Sunrise, Fajr, Dhuhr, Asr, Maghrib, Isha,gregorian, hijri);
+//     })
+//     .catch(error => console.log(error + ' @Getting Time Prayer Func'))
+// }
 
-  const [SunriseTime, FajrTime, DhuhrTime, AsrTime, MaghribTime, IshaTime] =
-    resultArr;
 
-  gregorianElement.innerHTML = gregorian
-  hijriElement.innerHTML = hijri
-  sunriseElement.innerHTML = SunriseTime;
-  fajrElement.innerHTML = FajrTime;
-  dhuhrElement.innerHTML = DhuhrTime;
-  asrElement.innerHTML = AsrTime;
-  maghribElement.innerHTML = MaghribTime;
-  ishaElement.innerHTML = IshaTime;
-}
+// function displayTimes(
+//   Sunrise,
+//   Fajr,
+//   Dhuhr,
+//   Asr,
+//   Maghrib,
+//   Isha,
+//   gregorian,
+//   hijri
+// ) {
+//   let sunriseElement = document.querySelector(".sunrise .time");
+//   let fajrElement = document.querySelector(".fajr .time");
+//   let dhuhrElement = document.querySelector(".dhuhr .time");
+//   let asrElement = document.querySelector(".asr .time");
+//   let maghribElement = document.querySelector(".maghrib .time");
+//   let ishaElement = document.querySelector(".isha .time");
+//   let gregorianElement = document.querySelector(".gregorian-date");
+//   let hijriElement = document.querySelector(".hijri-date");
 
-const search = document.querySelector(".search-logo")
-search.addEventListener("click", searchTimesOfTheCity)
+//   let arr = [Sunrise, Fajr, Dhuhr, Asr, Maghrib, Isha];
+//   const resultArr = arr.map((item) => {
+//     const hours = Number(item.slice(0, 2));
+//     const minutes = item.slice(2);
+//     return hours > 12 ? hours - 12 + minutes + " PM" : item + " AM";
+//   });
 
-function searchTimesOfTheCity (){
-  const input = document.querySelector(".search-input");
-  getTimesPrayer(input.value);
-}
+//   const [SunriseTime, FajrTime, DhuhrTime, AsrTime, MaghribTime, IshaTime] =
+//     resultArr;
+
+//   gregorianElement.innerHTML = gregorian
+//   hijriElement.innerHTML = hijri
+//   sunriseElement.innerHTML = SunriseTime;
+//   fajrElement.innerHTML = FajrTime;
+//   dhuhrElement.innerHTML = DhuhrTime;
+//   asrElement.innerHTML = AsrTime;
+//   maghribElement.innerHTML = MaghribTime;
+//   ishaElement.innerHTML = IshaTime;
+// }
+
+// const search = document.querySelector(".search-logo")
+// search.addEventListener("click", searchTimesOfTheCity)
+
+// function searchTimesOfTheCity (){
+//   const input = document.querySelector(".search-input");
+//   getTimesPrayer(input.value);
+// }
